db032a755b8b5521b7fd2b90b53976bf59e092af
Nuxnuxx <nuxnux02290@gmail.Com>
2022-09-26 16:51:35 +0200

    ajout

--- DevWeb/TP/TP2/index.php
+++ DevWeb/TP/TP2/index.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-type:text/html;charset=utf-8');
 $db = new PDO(
     'mysql:host=localhost;port=3306;dbname=R301',
     'phpmyadmin',
@@ -6,34 +7,39 @@ $db = new PDO(
 );
 
 $db->exec('SET NAMES UTF8');
-
 $artiste = file('artiste.csv');
 $album = file('album.csv');
 $genre = file('genre.csv');
 
 foreach ($artiste as $key => $value) {
     $artiste = explode(';', $value);
-    echo "key: " . $artiste[0] . "<br>";
-    echo "artiste: " . $artiste[1] . "<br>";
-    $db->exec("INSERT INTO artiste (id,nom) VALUES ($artiste[0],'$artiste[1]')");
+    $db->exec("INSERT INTO artiste (id,nom) 
+    VALUES ($artiste[0],'$artiste[1]')");
 }
 
 echo "<br>";
 foreach ($album as $key => $value) {
     $album = explode(';', $value);
-    echo "key: " . $album[0] . "<br>";
-    echo "nom: " . $album[1] . "<br>";
-    echo "genre: " . $album[2] . "<br>";
-    echo "artiste: " . $album[3] . "<br>";
-    echo "date: " . $album[4] . "<br>";
-    $db->exec("INSERT INTO album (id,nom, genre, artiste, date) VALUES ($album[0], $album[1], $album[2], $album[3], $album[4])");
+    $db->exec("INSERT INTO album (id, nom, genre, artiste, date) 
+    VALUES ($album[0],'$album[1]',$album[2],$album[3],'$album[4]')");
 }
 
 echo "<br>";
-
 foreach ($genre as $key => $value) {
     $genre = explode(';', $value);
-    echo "key: " . $genre[0] . "<br>";
-    echo "genre: " . $genre[1] . "<br>";
-    $db->exec("INSERT INTO genre (id,nom) VALUES ($genre[0],$genre[1])");
+    $db->exec("INSERT INTO genre (id,nom)
+    VALUES ($genre[0],'$genre[1]')");
 }
+
+echo "<table border='1'>";
+echo "<tr><th>id</th><th>nom</th><th>genre</th><th>artiste</th><th>date</th></tr>";
+foreach ($db->query('SELECT * FROM album') as $row) {
+    echo "<tr>";
+    echo "<td>" . $row['id'] . "</td>";
+    echo "<td>" . $row['nom'] . "</td>";
+    echo "<td>" . $db->query("SELECT nom FROM genre WHERE id = $row[genre]")->fetchColumn() . "</td>";
+    echo "<td>" . $db->query("SELECT nom FROM artiste WHERE id = $row[artiste]")->fetchColumn() . "</td>";
+    echo "<td>" . $row['date'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
